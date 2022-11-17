349a6cd839dc247aca571e27a0c60db2d8fbe547
Anthony Margerand <anthony@lbcd.fr>
2022-11-17 22:14:55 +0100

    fix: removed unnecessary text

--- src/Infrastructure/Command/ApiKeyGenerateCommand.php
+++ src/Infrastructure/Command/ApiKeyGenerateCommand.php
@@ -29,7 +29,7 @@ class ApiKeyGenerateCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         $apiKey = $this->apiKeyRepository->createNewApiKey();
-        $io->success(sprintf('Your key has beeen successfully generated : %s This key has a quota of %d', $apiKey->getKey()));
+        $io->success(sprintf('Your key has beeen successfully generated : %s', $apiKey->getKey()));
         $io->success(sprintf('This key has a quota of %d', $apiKey->getQuota()));
 
         return Command::SUCCESS;
